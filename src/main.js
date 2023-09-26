15d461e09971cda0f209ddcbec14331941d34c63
Timelordregeneration10 <3497049745@qq.com>
2023-09-26 12:21:28 +0800

    rmt

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import generate from "./components/happy.js";
-generate(0);
-
-(function () {
-    var parent = document.getElementsByClassName('contain-box')[0];
-    setInterval(function () {
-           parent.scrollTop+=2;
-    }, 3);
-})()
